b0e1e0cb508b434734c886389ea6abb135f422e4
darkArp <mario@whitehathacking.tech>
2020-12-03 16:50:49 +0100

    added one-liners

--- 2020/03/python/solution.py
+++ 2020/03/python/solution.py
@@ -1,4 +1,5 @@
 def task1_naive(map_input, up_slope, right_slope):
+    # Simple naive solution
     i = up_slope
     j = right_slope
     path_len = len(map_input[0]) - 1
@@ -12,6 +13,7 @@ def task1_naive(map_input, up_slope, right_slope):
 
 
 def task2_naive(map_input):
+    # Simple naive solution
     slopes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
     trees = 1
     for right_slope, up_slope in slopes:
@@ -19,8 +21,20 @@ def task2_naive(map_input):
     return trees
 
 
+def task1_one_line():
+    # One line solve
+    from math import prod;print(prod([sum(row[i*3%len(row.strip())]=="#"for i,row in enumerate(open('map.txt','r')))]))
+
+
+def task2_one_line():
+    # One line solve
+    from math import prod;print(prod([sum(row[i//down*right%len(row.strip())]=="#" for i,row in enumerate(open('map.txt', 'r'))if i%down==0) for right,down in ((1,1),(3,1),(5,1),(7,1),(1,2))]))
+
+
 if __name__ == "__main__":
     map_input = [line for line in open("map.txt", "r")]
     task1 = task1_naive(map_input, 1, 3)
-    task2 = task2_naive(map_input)
-    print(task1, task2)
+    task2_1 = task2_naive(map_input)
+    print(task1, task2_1)
+    task1_one_line()
+    task2_one_line()
