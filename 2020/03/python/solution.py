b7f037b279bb9d7a86d34e4b2f6b3fc1d8640fda
darkArp <mario@whitehathacking.tech>
2020-12-03 17:14:00 +0100

    comments

--- 2020/03/python/solution.py
+++ 2020/03/python/solution.py
@@ -1,3 +1,4 @@
+# Task 1
 def task1_naive(map_input, up_slope, right_slope):
     # Simple naive solution
     i = up_slope
@@ -12,6 +13,15 @@ def task1_naive(map_input, up_slope, right_slope):
     return trees
 
 
+
+
+
+def task1_one_line():
+    # One line solve
+    from math import prod;print(prod([sum(row[i*3%len(row.strip())]=="#"for i,row in enumerate(open('map.txt','r')))]))
+
+
+# Task 2
 def task2_naive(map_input):
     # Simple naive solution
     slopes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
@@ -21,14 +31,9 @@ def task2_naive(map_input):
     return trees
 
 
-def task1_one_line():
-    # One line solve
-    from math import prod;print(prod([sum(row[i*3%len(row.strip())]=="#"for i,row in enumerate(open('map.txt','r')))]))
-
-
 def task2_one_line():
     # One line solve
-    from math import prod;print(prod([sum(row[i//down*right%len(row.strip())]=="#" for i,row in enumerate(open('map.txt', 'r'))if i%down==0) for right,down in ((1,1),(3,1),(5,1),(7,1),(1,2))]))
+    from math import prod;print(prod((sum(row[i//down*right%len(row.strip())]=="#"for i,row in enumerate(open('map.txt', 'r'))if i%down==0)for right,down in((1,1),(3,1),(5,1),(7,1),(1,2)))))
 
 
 if __name__ == "__main__":
