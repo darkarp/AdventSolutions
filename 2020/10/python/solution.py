79911e3d176bb15c379d20ef06fc92480a585971
darkArp <mario@whitehathacking.tech>
2020-12-10 15:41:26 +0100

    Added credit repo

--- 2020/10/python/solution.py
+++ 2020/10/python/solution.py
@@ -38,9 +38,10 @@ def count_differences(outlet_list: set, outlet_charge: int) -> int:
     return diff_counter_1 * diff_counter_3
 
 
-def count_arrangements(outlet_list: set, outlet_charge: int):
+def count_arrangements(outlet_list: set, outlet_charge: int) -> int:
     # Count total paths based on number of posibilities of each number
     # Credits to Francisco Traquete for heling me out with this one.
+    # https://github.com/Francisco-T
     # My previous solution was very time and space inefficient
     diff_dict = {outlet_charge: 1}
     for number in outlet_list:
